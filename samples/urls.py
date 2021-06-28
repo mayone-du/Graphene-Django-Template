1909510d96794794b21f2b3247a1e8df72df1bbf
mayone-du <cocomayo1201@icloud.com>
2021-06-28 16:26:39 +0900

    Fix name scheme -> schema, prj -> project

--- samples/urls.py
+++ samples/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
-from prj.schema import schema
+from project.schema import schema
 from django.views.decorators.csrf import csrf_exempt
 from graphene_file_upload.django import FileUploadGraphQLView
 from django.conf.urls.static import static
@@ -12,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True, schema=schema))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
